674bf065fab3d9f5d7ea810817514e81b9b8c391
Leisan Kokorina <leisan.kokorina.001@student.uni.lu>
2023-05-30 23:23:02 +0200

    minor changes

--- sales_by_country.py
+++ sales_by_country.py
@@ -12,19 +12,19 @@ country_sales = data.groupby('Country')['Sales'].sum().sort_values(ascending=Fal
 country_sales['Percentage'] = (country_sales['Sales'] / country_sales['Sales'].sum()) * 100
 
 # Create the percentage plot
-fig = px.bar(country_sales, x='Country', y='Percentage', color='Country', title='Percentage of Total Sales by Country')
-fig.show()
+fig1 = px.bar(country_sales, x='Country', y='Percentage', color='Country', title='Percentage of Total Sales by Country')
+fig1.show()
 
 # Select top 10 countries
 top_10_countries = country_sales.head(10)
 
 # Create the pie chart
-fig = px.pie(top_10_countries, values='Percentage', names='Country', title='Percentage of Total Sales by Country')
+fig2 = px.pie(top_10_countries, values='Percentage', names='Country', title='Percentage of Total Sales by Country')
 
 # Add percentage labels inside the sectors
-fig.update_traces(textposition='inside', textinfo='percent+label')
+fig2.update_traces(textposition='inside', textinfo='percent+label')
 
-fig.show()
+fig2.show()
 
 
 
