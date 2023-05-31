a6f0944aba1c371cac01ab0276566dd14f1209fc
Leisan Kokorina <leisan.kokorina.001@student.uni.lu>
2023-05-31 21:17:03 +0200

    updating sales_by_country.py

--- sales_by_country.py
+++ sales_by_country.py
@@ -8,26 +8,17 @@ data = pd.read_excel('cleaned_data.xlsx')
 data['Sales'] = data['Quantity'] * data['UnitPrice']
 country_sales = data.groupby('Country')['Sales'].sum().sort_values(ascending=False).reset_index()
 
-# Calculate percentage of sales
+# Calculate percentage of total sales for each country
 country_sales['Percentage'] = (country_sales['Sales'] / country_sales['Sales'].sum()) * 100
 
 # Create the percentage plot
 fig1 = px.bar(country_sales, x='Country', y='Percentage', color='Country', title='Percentage of Total Sales by Country')
 fig1.show()
 
-# Select top 10 countries
-top_10_countries = country_sales.head(10)
-
-# Create the pie chart
-fig2 = px.pie(top_10_countries, values='Percentage', names='Country', title='Percentage of Total Sales by Country')
+# Create the percentage pie chart
+fig2 = px.pie(country_sales, values='Percentage', names='Country', title='Percentage of Total Sales by Country')
 
 # Add percentage labels inside the sectors
 fig2.update_traces(textposition='inside', textinfo='percent+label')
 
 fig2.show()
-
-
-
-
-
-
