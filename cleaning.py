ee58705c835e389077e91cdd388b63b7283ef4bb
Leisan Kokorina <leisan.kokorina.001@student.uni.lu>
2023-05-31 16:59:55 +0200

    updating cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -15,29 +15,15 @@ data[['InvoiceDate', 'InvoiceTime']] = data['InvoiceDate'].str.split(pat=' ', n=
 # Move 'InvoiceTime' column right after 'InvoiceDate'
 data.insert(data.columns.get_loc('InvoiceDate') + 1, 'InvoiceTime', data.pop('InvoiceTime'))
 
-# Find the maximum CustomerID
-max_customer_id = data['CustomerID'].max()
-
-# Create a dictionary to store the mapping of unique InvoiceNo to a new CustomerID
-unique_invoice_mapping = {}
-
-# Generate unique CustomerIDs for missing values based on unique InvoiceNo
-current_customer_id = max_customer_id + 1
-for index, row in data.iterrows():
-    invoice_no = row['InvoiceNo']
-    customer_id = row['CustomerID']
-    if pd.isnull(customer_id):
-        if invoice_no not in unique_invoice_mapping:
-            unique_invoice_mapping[invoice_no] = current_customer_id
-            current_customer_id += 1
-
-# Update the CustomerID column with the new unique CustomerIDs
-data['CustomerID'] = data.apply(lambda row: unique_invoice_mapping[row['InvoiceNo']]
-if pd.isnull(row['CustomerID']) else row['CustomerID'], axis=1)
+# Remove data without CustomerID
+data = data[~(data.CustomerID.isnull())]
 
 # Fill empty cells in Description column based on StockCode
 data['Description'] = data.groupby('StockCode')['Description'].transform(
     lambda x: x.fillna(x.mode().iloc[0]) if not x.mode().empty else '')
+
+# Drop rows with blank Description
+data = data[data['Description'] != '']
 
 # Trim values in Description column
 data['Description'] = data['Description'].str.strip(' ,.')
@@ -45,14 +31,17 @@ data['Description'] = data['Description'].str.strip(' ,.')
 # Remove extra whitespace in the "Description" column
 data['Description'] = data['Description'].str.replace(r'\s+', ' ', regex=True)
 
-# Find duplicate rows based on all columns except "Quantity"
-duplicate_rows = data.duplicated(subset=data.columns.difference(['Quantity']), keep='first')
+# Convert 'InvoiceNo' column to string
+data['InvoiceNo'] = data['InvoiceNo'].astype('str')
 
-# Update the "Quantity" column for unique rows by summing the values
-data['Quantity'] = data.groupby(data.columns.tolist()[:-1])['Quantity'].transform('sum')
+# Remove rows with InvoiceNo starting with 'C' (indicating cancellations)
+data = data[~data['InvoiceNo'].str.contains('C')]
 
-# Remove the duplicate rows
-data = data[~duplicate_rows]
+# Remove rows with negative or equal to 0 UnitPrice
+data = data[~(data["UnitPrice"] <= 0)]
+
+# Remove rows with negative Quantity
+data = data[~(data["Quantity"] < 0)]
 
 # Save the updated DataFrame to a new Excel file
 data.to_excel('cleaned_data.xlsx', index=False)
